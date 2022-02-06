aa9e9b2f25a90193a49cdf7f781c73de4eeb7423
Wiktor Łazarski <wjlazarski@gmail.com>
2022-02-06 18:51:57 +0100

    Image loading and augmentation

---
+++ scripts/training/data_loading.py
@@ -0,0 +1,56 @@
+import pathlib as p
+import typing as t
+
+import cv2
+import torch
+import torchvision
+
+
+class CelebAHeadSegmentationDataset(torchvision.datasets.VisionDataset):
+    def __init__(
+        self,
+        dataset_path: str,
+        augmentation_pipeline: t.Callable = None,
+        preprocess_pipeline: t.Callable = None,
+    ):
+        super().__init__(root=dataset_path)
+
+        self.image_files = self.__load_image_files()
+        self.aug_pipeline = augmentation_pipeline
+        self.preprocess_pipeline = preprocess_pipeline
+
+    def __len__(self) -> int:
+        return len([])
+
+    def __getitem__(self, index: int) -> t.Tuple[torch.Tensor, torch.Tensor]:
+        image_file = self.image_files[index]
+
+        image, segmap = self.__load_sample(image_file)
+
+        if self.aug_pipeline is not None:
+            image, segmap = self.aug_pipeline(image=image, segmap=segmap)
+
+        if self.preprocess_pipeline is not None:
+            image = self.preprocess_pipeline(img=image)
+
+        return image, segmap
+
+    def __load_image_files(self) -> t.List[str]:
+        images_dir = p.Path(self.root) / "images"
+
+        image_files = sorted(list(images_dir.glob("*.jpg")))
+
+        return image_files
+
+    def __load_sample(
+        self, image_filename: p.Path
+    ) -> t.Tuple[torch.Tensor, torch.Tensor]:
+        image = cv2.imread(str(image_filename), cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        segmap_file = (
+            image_filename.parent.parent / "segmaps" / f"{image_filename.stem}.png"
+        )
+        segmap = cv2.imread(str(segmap_file), cv2.IMREAD_GRAYSCALE)
+
+        return image, segmap
