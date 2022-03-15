ea596a8f659a5e38ad25f102dfab07982b8cfb75
Wiktor Łazarski <wjlazarski@gmail.com>
2022-03-15 08:11:04 +0100

    Models stored in repo with Git LFS and package installation with model checkpoint.

--- head_segmentation/constants.py
+++ head_segmentation/constants.py
@@ -1,4 +1,15 @@
+import os
+import typing as t
+
 ###### DATASET ######
 
 LABEL2INDEX = {"background": 0, "head": 1}
 INDEX2LABEL = {0: "background", 1: "head"}
+
+###### MODEL ######
+
+HEAD_SEGMENTATION_MODEL_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "model",
+    "best_model_from_experiments.ckpt",
+)
