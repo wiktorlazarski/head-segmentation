42d744b582830dfd242ef940b215f773cc28e5af
Szuumii <jakub.szumskii@gmail.com>
2022-03-30 19:48:26 +0200

    Prediction and visualization pipelines

---
+++ head_segmentation/predict_pipeline.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import torch
+
+import head_segmentation.constants as C
+import head_segmentation.image_processing as ip
+import head_segmentation.model as mdl
+
+
+class HumanHeadSegmentationPipeline:
+    def __init__(
+        self,
+        model_path: str = C.HEAD_SEGMENTATION_MODEL_PATH,
+        image_input_resolution: int = 512,
+    ):
+        self._preprocessing_pipeline = ip.PreprocessingPipeline(
+            nn_image_input_resolution=image_input_resolution
+        )
+        self._model = mdl.HeadSegmentationModel.load_from_checkpoint(
+            ckpt_path=model_path
+        )
+        self._model.eval()
+
+    def _preprocess_image(self, image: np.ndarray) -> torch.Tensor:
+        preprocessed_image = self._preprocessing_pipeline.preprocess_image(image)
+        preprocessed_image = preprocessed_image.unsqueeze(0)
+        return preprocessed_image
+
+    def _postprocess_model_output(
+        self, out: torch.Tensor, original_image: np.ndarray
+    ) -> np.ndarray:
+        out = out.squeeze()
+        out = out.argmax(dim=0)
+        out = out.numpy().astype(np.uint8)
+        h, w = original_image.shape[:2]
+        postprocessed = cv2.resize(out, (w, h), interpolation=cv2.INTER_NEAREST)
+
+        return postprocessed
+
+    def predict(self, image: np.ndarray) -> np.ndarray:
+        preprocessed_image = self._preprocess_image(image)
+        mdl_out = self._model(preprocessed_image)
+        pred_segmap = self._postprocess_model_output(mdl_out, original_image=image)
+        return pred_segmap
---
+++ head_segmentation/visualization.py
@@ -0,0 +1,47 @@
+import typing as t
+
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class VisualizationModule:
+    def __init__(
+        self,
+        figsize: t.Tuple[int, int] = (7, 5),
+        font_size: int = 16,
+    ):
+        self.figsize = figsize
+        self.font_size = font_size
+
+        self.cmap = "gray"
+
+    def visualize_image(self, image: np.ndarray) -> matplotlib.figure.Figure:
+        f = plt.figure(figsize=self.figsize)
+        plt.imshow(image, cmap=self.cmap)
+        return f
+
+    def visualize_prediction(
+        self, image: np.ndarray, pred_segmap: np.ndarray, save_images=False
+    ) -> t.Tuple[matplotlib.figure.Figure, matplotlib.axis.Axes]:
+        segmented_region = image * cv2.cvtColor(pred_segmap, cv2.COLOR_GRAY2RGB)
+
+        figsize = (3 * self.figsize[0], 2 * self.figsize[1])
+
+        f, ax = plt.subplots(1, 3, figsize=figsize)
+        f.tight_layout()
+
+        ax[0].imshow(image)
+        ax[0].set_title("Input image", fontsize=self.font_size)
+
+        ax[1].imshow(pred_segmap, cmap=self.cmap)
+        ax[1].set_title("Predicted segmentation map", fontsize=self.font_size)
+
+        ax[2].imshow(segmented_region)
+        ax[2].set_title("Segmented region image", fontsize=self.font_size)
+
+        if save_images:
+            plt.savefig("out.png")
+
+        return f, ax
